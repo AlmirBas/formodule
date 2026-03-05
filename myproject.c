32d2a1e78e4f8d64ed5def588c44d96be97894da
Amir Baş <almirbas128@gmail.com>
2026-03-05 15:00:21 +0300

    myproject düzenlendi.

--- myproject.c
+++ myproject.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int max(int Liste[], int size){
+    int maxv=-1;
+    for(int i=0; i < size; i++){
+        if(Liste[i]>maxv){
+            maxv = Liste[i];
+        }
+    }
+    return maxv;
+}
+
+
+
 void main(void){
 
-    printf("Hello \n");
-    printf("World! \n");
+    int Liste[] = {2, 3, 4, 4, 66, 65, 3575, 6637, 737375};
+    int size = sizeof(Liste)/sizeof(Liste[0]);
+    int c = max(Liste, size);
+    printf("%i", c);
     
 }
